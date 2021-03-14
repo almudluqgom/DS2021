049b3a58e7f8db068798f6fc2eae4b0715a37069
Almudena Luque <61197937+almudluqgom@users.noreply.github.com>
2021-03-14 11:18:33 +0100

    Add files via upload

---
+++ PR1/src/ObservadorConsumoPatatas.java
@@ -0,0 +1,17 @@
+import java.util.Observable;
+import java.util.Observer;
+
+public class ObservadorConsumoPatatas implements Observer {
+
+	/**
+	 * 
+	 * @param o
+	 * @param arg
+	 */
+        NPatataFrame p;
+	public void update(Observable o, Object arg) {
+		// TODO - implement ObservadorConsumoPatatas.update
+		throw new UnsupportedOperationException();
+	}
+
+}
